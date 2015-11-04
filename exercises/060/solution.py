3929c798d041087700a5ae5f61fa1360b8ee7d50
Quendil <p.pong582@laposte.net>
2015-11-04 18:37:23 +0100

    commit

---
+++ exercises/060/solution.py
@@ -0,0 +1,5 @@
+import operator
+a = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+for i in a:
+    for j in a:
+        print(operator.add(str(i), str(j)))
