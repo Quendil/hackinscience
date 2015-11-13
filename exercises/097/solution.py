fbb597cd3fd5775aea9b89badb24a0f256ad2073
Quendil <p.pong582@laposte.net>
2015-11-13 12:16:57 +0100

    commit

--- exercises/097/solution.py
+++ exercises/097/solution.py
@@ -1,6 +1,6 @@
 def love_meet(l, m):
-    print(set(l).intersection(set(m)))
+    return(set(l).intersection(set(m)))
 
 
 def affair_meet(l, m, n):
-    print(set(m).intersection(set(n)).difference(set(l)))
+    return(set(m).intersection(set(n)).difference(set(l)))
