6a1ab3a0d115d551709b55a0c3375da97a03c382
Quendil <p.pong582@laposte.net>
2015-11-13 12:33:55 +0100

    commit

--- exercises/100/solution.py
+++ exercises/100/solution.py
@@ -1,10 +1,6 @@
-station = {
- 'address': 'RUE DES CHAMPEAUX (PRES DE LA GARE ROUTIERE) - 93170 BAGNOLET',
- 'number': 31705,
- 'latitude': 48.8645278209514,
- 'name': 'CHAMPEAUX (BAGNOLET)',
- 'longitude': 2.416170724425901
-}
+station = {'address': 'RUE DES CHAMPEAUX (PRES DE LA GARE ROUTIERE) - 93170 \
+BAGNOLET', 'number': 31705, 'latitude': 48.8645278209514, 'name': 'CHAMPEAUX \
+(BAGNOLET)', 'longitude': 2.416170724425901}
 print('latitude', station['latitude'])
 print('longitude', station['longitude'])
 print('number', station['number'])
