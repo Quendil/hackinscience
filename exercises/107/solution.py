6ea3931c4d02ca73d1b5e2876291d068ee7a8ae0
Quendil <p.pong582@laposte.net>
2015-11-27 14:43:57 +0100

    commit

---
+++ exercises/107/solution.py
@@ -0,0 +1,14 @@
+def select_student(l, v):
+    a = []
+    b = []
+    import operator
+    d = dict(l)
+    for i in d:
+        if d[i] > v:
+            a.append(i)
+            a.append(d[i])            
+        else:
+            b.append(i)
+            b.append(d[i])
+    print(dict([('Accepted', a)]))
+    print(dict([('Refused', b)]))
