a2573296fa5530e17e0fd5f6e07ab23b3516c817
Quendil <p.pong582@laposte.net>
2015-11-27 16:59:50 +0100

    commit

--- exercises/107/solution.py
+++ exercises/107/solution.py
@@ -3,26 +3,28 @@ def select_student(l, v):
     b = []
     c = []
     d = []
-    y = []
-    z = []
+    e = []
+    f = []
     import operator
-    e = dict(l)
-    for i in e:
-        if e[i] > v:
+    z = dict(l)
+    for i in z:
+        if z[i] > v:
             a.append(i)
-            b.append(e[i])
+            b.append(z[i])
         else:
             c.append(i)
-            d.append(e[i])
+            d.append(z[i])
     m = a[0], b[0]
     n = c[0], d[0]
-    y.append(list(m))
-    z.append(list(n))
+    e.append(list(m))
+    f.append(list(n))
     for i in range(1, len(a)):
         m = a[i], b[i]
-        y.append(list(m))
+        e.append(list(m))
     for i in range(0, len(c)):
         n = c[i], d[i]
-        z.append(list(n))
-    print('Accepted', y)
-    print('Refused', z)
+        f.append(list(n))
+    g = 'Accepted', list(e)
+    h = 'Refused', list(f)
+    print(g)
+    print(h)
