1e439808e27787a01e6c8f573a98d28e946266a5
Quendil <p.pong582@laposte.net>
2015-11-27 17:51:47 +0100

    commit

--- exercises/107/solution.py
+++ exercises/107/solution.py
@@ -22,6 +22,5 @@ def select_student(l, v):
         f.append(list(n))
     e.sort(reverse=True, key=operator.itemgetter(1))
     f.sort(key=operator.itemgetter(1))
-    g = {'Accepted': list(e), \
-    'Refused': list(f)}
+    g = {'Accepted': list(e), 'Refused': list(f)}
     return(g)
