e226f6be985dee7775a7cb08d2ea03409f3be112
Quendil <p.pong582@laposte.net>
2015-12-04 16:24:45 +0100

    commit

--- exercises/200/solution.py
+++ exercises/200/solution.py
@@ -1,9 +1,11 @@
 def is_prime(x):
     n = 2
     s = 0
+    if x == 1:
+        return(False)
     if x == 2:
         return(True)
-    else:
+    if x > 2:
         while n < x:
             if x % n == 0:
                 s = 1
