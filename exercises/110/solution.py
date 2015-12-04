5cd34751428425bb1fbb202f19f0c9c907561da9
Quendil <p.pong582@laposte.net>
2015-12-04 15:45:25 +0100

    commit

---
+++ exercises/110/solution.py
@@ -0,0 +1,19 @@
+import sys
+if len(sys.argv) == 4:
+    if sys.argv[2] == '+':
+        print(int(sys.argv[1]) + int(sys.argv[3]))
+    if sys.argv[2] == '-':
+        print(int(sys.argv[1]) - int(sys.argv[3]))
+    if sys.argv[2] == '*':
+        print(int(sys.argv[1]) * int(sys.argv[3]))
+    if sys.argv[2] == '/':
+        if sys.argv[3] == '0':
+            print('input error')
+        else:
+            print(int(sys.argv[1]) / int(sys.argv[3]))
+    if sys.argv[2] == '%':
+        print(int(sys.argv[1]) % int(sys.argv[3]))
+    if sys.argv[2] == '^':
+        print(int(sys.argv[1]) ** int(sys.argv[3]))
+else:
+    print('usage: ./solution.py a_number (an_operator +-*/%^) a_number')
